7b1815846cd58a413a3f085e980a8d2e9ebf0e12
Kevin Perry <kevinperry@gatech.edu>
2019-01-27 19:18:33 -0500

    Updates

---
+++ precache-manifest.0834b15530a62118171faea77412d5ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4a38b477ffe96f10a6b",
+    "url": "/colorace/static/js/runtime~main.f4a38b47.js"
+  },
+  {
+    "revision": "39475a701bc8168216da",
+    "url": "/colorace/static/js/main.39475a70.chunk.js"
+  },
+  {
+    "revision": "936684c419042b396f13",
+    "url": "/colorace/static/js/1.936684c4.chunk.js"
+  },
+  {
+    "revision": "39475a701bc8168216da",
+    "url": "/colorace/static/css/main.1208bccd.chunk.css"
+  },
+  {
+    "revision": "5a104f87e60706cd6ac2c21e29c4adba",
+    "url": "/colorace/index.html"
+  }
+];
